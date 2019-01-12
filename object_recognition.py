e60336bfe4ff224fde0a179462e838cc7dbf1a99
tdksr0505 <tdksr0505@gmail.com>
2019-01-12 16:41:00 +0800

    Update object_recognition.py

--- object_recognition.py
+++ object_recognition.py
@@ -47,12 +47,10 @@ def recognize_image():
     with tf.Session() as sess:
         
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-
-        
+     
         predictions = sess.run(softmax_tensor, \
                  {'DecodeJpeg/contents:0': image_data})       
-        
-        
+      
         top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
         # for node_id in top_k:
